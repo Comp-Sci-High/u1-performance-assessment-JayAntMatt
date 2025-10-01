00cb436c668349c7be09cc1105d9f62828e862d4
JayAntMatt <jaylen.matthews26@compscihigh.org>
2025-10-01 16:47:09 +0000

    Raccoons are the best. What a great pet.

--- index.js
+++ index.js
@@ -69,15 +69,14 @@ console.log(afterSchoolClubs)
 // 4th Floor
 // Uncomment the code below to begin this task
 
-// let takeCareOfPet = function(snacks, pets) => {
-//     if (Snacks[2] === "hotdogs" && pets > 10) {
-//         return "Raccoon has been appeased"
-//     } else {
-//         return "The raccoon bit you and you died of rabies."
-    
-// }
-
-
+const takeCareOfPet = (snacks, pets) => {
+if (snacks[2] === "hotdogs" && pets > 10) {
+return "Raccoon has been appeased"
+} else {
+return "The raccoon bit you and you died of rabies."
+}
+console.log(takeCareOfPet(snacks,pets))
+}
 // Commit and show us to unlock the next floor's instructions
 // ----------------------------
 // 5th Floor
